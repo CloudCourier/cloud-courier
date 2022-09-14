91e6e9971a1a790c4e6b4e16302c2f117c61174d
zbl <59828992+zbl666@users.noreply.github.com>
2022-09-15 02:05:15 +0800

    chore: 修改脚本url

--- src/pages/Workbench/MySubject/DeployModal.tsx
+++ src/pages/Workbench/MySubject/DeployModal.tsx
@@ -23,7 +23,7 @@ const DeployModal: FC<DeployModalIF> = props => {
         s0 = document.getElementsByTagName('script')[0];
       s1.async = true;
       s1.src =
-        '${DOMAINNAME}/js/deployscript.bundle.js';
+        '${DOMAINNAME}/scripts/deployscript.js';
       s1.charset = 'UTF-8';
       s1.setAttribute('crossorigin', '*');
       s0.parentNode.insertBefore(s1, s0);
